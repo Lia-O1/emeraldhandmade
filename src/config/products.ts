04e71a8ffa8e33e78634882690ddd9afee2072d7
Lia-O1 <133197997+Lia-O1@users.noreply.github.com>
2024-02-13 22:39:41 +1100

    Updated the `products.ts`
    - Added ids
    - This data is decided to be hardcoded since this is a small frontend project

--- src/config/products.ts
+++ src/config/products.ts
@@ -4,22 +4,26 @@ export const products = [
     subcategory: "bestsellers",
     items: [
       {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
+        name: "Lavender Bliss Artisan Soap",
+        id: "lavenderSoap",
+        price: "5",
+        description:
+          "Indulge in the luxurious embrace of our Lavender Bliss Artisan Soap. Handcrafted with love, each bar is a masterpiece of nourishing oils and butter that tenderly cleanse and pamper your skin. The serene purple hue, achieved with natural colorants, is a visual testament to its calming properties. Adorned with dried lavender blossoms, this soap not only offers a soothing aroma but also acts as a gentle exfoliator, revealing radiant and refreshed skin with every use. Elevate your daily cleansing ritual to an enriching experience of pure bliss.",
+        urls: [
+          "/images/products/health-beauty/lavender-soap-primary.jpg",
+          "/images/products/health-beauty/lavender-soap-secondary.jpg",
+        ],
       },
       {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
-      },
-      {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
+        name: "Artisanal Soap Collection",
+        id: "soapCollection",
+        price: "16",
+        description:
+          "This collection features four unique artisanal soaps, each with its own charm. The top soap is white with dried flowers, offering a gentle, floral touch. The second soap is a smooth, pink bar, simple yet elegant. The third soap showcases a marbled pattern in dark hues, adding a touch of sophistication. The bottom soap is speckled with various colors and textures, reflecting the richness of its ingredients. This collection offers a soap for every mood and preference.",
+        urls: [
+          "/images/products/health-beauty/soap-collection-primary.jpg",
+          "/images/products/health-beauty/soap-collection-secondary.jpg",
+        ],
       },
     ],
   },
@@ -28,22 +32,15 @@ export const products = [
     subcategory: "new-arrivals",
     items: [
       {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
-      },
-      {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
-      },
-      {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
+        name: "Vibrant Harmony Herbal Tea",
+        id: "teaBlend",
+        price: "20",
+        description:
+          "Experience the soothing symphony of our Vibrant Harmony Herbal Tea. This blend features a variety of dried flowers, leaves, and fruit slices, offering a burst of flavors with every sip. Perfect for a calming tea time experience. Ingredients: chamomile flowers, hibiscus petals, rose hips, or citrus peels. Weight: 200g per bag.",
+        urls: [
+          "/images/products/health-beauty/tea-blend-primary.jpg",
+          "/images/products/health-beauty/tea-blend-secondary.jpg",
+        ],
       },
     ],
   },
@@ -52,22 +49,26 @@ export const products = [
     subcategory: "editor-pics",
     items: [
       {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
+        name: "Serene Floral Essence Oil",
+        id: "floralOil",
+        price: "15",
+        description:
+          "Experience the soothing touch of our Serene Floral Essence Oil. Housed in an amber-colored glass bottle, this rosehip oil captures the essence of various flowers, offering a rejuvenating experience. Perfect for a calming self-care ritual.",
+        urls: [
+          "/images/products/health-beauty/floral-oil-primary.jpg",
+          "/images/products/health-beauty/floral-oil-secondary.jpg",
+        ],
       },
       {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
-      },
-      {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
+        name: "Chamomile Calm Essence",
+        id: "chamomileEssence",
+        price: "16",
+        description:
+          "Immerse yourself in tranquility with our Chamomile Calm Essence. Housed in a sleek, transparent dropper bottle, this essence captures the soothing properties of chamomile. Each drop delivers a sense of calm and relaxation, perfect for unwinding after a long day. Incorporate it into your evening routine to promote restful sleep and wake up feeling refreshed and rejuvenated.",
+        urls: [
+          "/images/products/health-beauty/chamomile-essence-primary.jpg",
+          "/images/products/health-beauty/chamomile-essence-secondary.jpg",
+        ],
       },
     ],
   },
@@ -76,22 +77,37 @@ export const products = [
     subcategory: "bestsellers",
     items: [
       {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
+        name: "Serene Glow Body Candle",
+        id: "bodyCandle",
+        price: "10",
+        description:
+          "Illuminate your space with our Serene Glow Body Candle. Shaped like a torso, this unique candle casts a gentle, calming light. Its placement on a reflective surface enhances the warm glow, creating a tranquil ambiance. Perfect for relaxation and meditation sessions.",
+        urls: [
+          "/images/products/home-living/body-candle-primary.jpg",
+          "/images/products/home-living/body-candle-secondary.jpg",
+        ],
       },
       {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
+        name: "Rustic Bull Head Clay Pot",
+        id: "bullPot",
+        price: "50",
+        description:
+          "Discover the unique charm of our Rustic Cow Head Clay Pot. This distinctive pot, shaped like a bull’s head, brings a touch of rustic whimsy to any space. The textured surface and earthy brown color of the pot evoke a pastoral appeal. Perfect for adding a unique artistic touch to your home or garden.",
+        urls: [
+          "/images/products/home-living/bull-pot-primary.jpg",
+          "/images/products/home-living/bull-pot-secondary.jpg",
+        ],
       },
       {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
+        name: "Floral Embroidery Fabric Bag",
+        id: "floralBag",
+        price: "25",
+        description:
+          "This hand-held fabric bag, adorned with vibrant floral embroidery, offers a blend of style and functionality. Its intricate design, featuring a variety of flowers and leaves, adds a touch of nature-inspired beauty to your ensemble. Perfect for carrying your essentials with elegance.",
+        urls: [
+          "/images/products/home-living/floral-bag-primary.jpg",
+          "/images/products/home-living/floral-bag-secondary.jpg",
+        ],
       },
     ],
   },
@@ -100,22 +116,26 @@ export const products = [
     subcategory: "new-arrivals",
     items: [
       {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
+        name: "Enchanted Owl Clay Pot",
+        id: "owlPot",
+        price: "50",
+        description:
+          "Discover the charm of our Enchanted Owl Clay Pot. This exquisite piece, meticulously crafted to resemble an owl, is a testament to rustic elegance. The pot’s intricate details, from the captivating eyes to the delicate floral embellishments, bring the enchanting essence of nature to your space. Perfect for adding a unique artistic touch to your home or garden.",
+        urls: [
+          "/images/products/home-living/owl-pot-primary.jpg",
+          "/images/products/home-living/owl-pot-secondary.jpg",
+        ],
       },
       {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
-      },
-      {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
+        name: "Rustic Elegance Pottery Vase",
+        id: "plainPot",
+        price: "45",
+        description:
+          "Experience the charm of our Rustic Elegance Pottery Vase. This large vase, with its earthy tones and textured surface, brings a touch of rustic beauty to any space. The vase’s size and unique texture make it a standout piece, perfect for adding a touch of artistic elegance to your home or garden.",
+        urls: [
+          "/images/products/home-living/plain-pot-primary.jpg",
+          "/images/products/home-living/plain-pot-secondary.jpg",
+        ],
       },
     ],
   },
@@ -124,22 +144,26 @@ export const products = [
     subcategory: "editor-pics",
     items: [
       {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
+        name: "Elegant Affection Candle Set",
+        id: "romanticCandles",
+        price: "18",
+        description:
+          "Our Elegant Affection Candle Set is a unique addition to any space. The set features two distinct candles: one shaped like a heart, embodying love and warmth, and the other resembling a playful cluster of bubbles. Both candles are soft pink, adding a touch of color and tranquility to your surroundings. Perfect for creating a serene and affectionate atmosphere.",
+        urls: [
+          "/images/products/home-living/romantic-candles-primary.jpg",
+          "/images/products/home-living/romantic-candles-secondary.jpg",
+        ],
       },
       {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
-      },
-      {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
+        name: "Floral Harmony Artisan Candle (Large)",
+        id: "floralCandle",
+        price: "18",
+        description:
+          "Our Floral Harmony Artisan Candles are a feast for the senses. Each candle, adorned with dried flowers and petals, creates a visually appealing aesthetic. The large centerpiece candle, surrounded by smaller ones, forms a beautiful arrangement. Perfect for adding a rustic and cozy ambiance to your space.",
+        urls: [
+          "/images/products/home-living/floral-candle-primary.jpg",
+          "/images/products/home-living/floral-candle-secondary.jpg",
+        ],
       },
     ],
   },
@@ -148,22 +172,26 @@ export const products = [
     subcategory: "bestsellers",
     items: [
       {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
+        name: "Golden Twisted Hoop Earrings",
+        id: "goldenEarrings",
+        price: "30",
+        description:
+          "Embrace elegance with our Golden Twisted Hoop Earrings. These intricately designed earrings, with their glossy finish, reflect light beautifully, adding a touch of sophistication to any outfit. Perfect for both casual and formal occasions, these earrings are a versatile addition to your jewelry collection.",
+        urls: [
+          "/images/products/jewelry-accessories/golden-earrings-primary.jpg",
+          "/images/products/jewelry-accessories/golden-earrings-secondary.jpg",
+        ],
       },
       {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
-      },
-      {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
+        name: "Golden Sphere and White Circle Earrings",
+        id: "whiteEarrings",
+        price: "25",
+        description:
+          "Add a touch of elegance to your look with our Golden Sphere and White Circle Earrings. The earrings feature a shiny golden sphere on top and a matte white circle at the bottom, creating a beautiful contrast. Their unique design makes them a versatile accessory, perfect for both casual and formal occasions.",
+        urls: [
+          "/images/products/jewelry-accessories/white-earrings-primary.jpg",
+          "/images/products/jewelry-accessories/white-earrings-secondary.jpg",
+        ],
       },
     ],
   },
@@ -172,22 +200,16 @@ export const products = [
     subcategory: "new-arrivals",
     items: [
       {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
-      },
-      {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
-      },
-      {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
+        name: "Glittery Dual-Tone Leaf Earrings",
+        id: "chamomileEssence",
+        price: "25",
+        description:
+          "Add a touch of sparkle to your look with our Glittery Dual-Tone Leaf Earrings. The upper part of these earrings features a glittery texture that beautifully catches the light, while the lower part has a smooth, dark finish for a striking contrast. Perfect for both casual and formal occasions, these earrings are a versatile addition to your jewelry collection.",
+
+        urls: [
+          "/images/products/jewelry-accessories/leather-earrings-primary.jpg",
+          "/images/products/jewelry-accessories/leather-earrings-secondary.jpg",
+        ],
       },
     ],
   },
@@ -196,22 +218,15 @@ export const products = [
     subcategory: "editor-pics",
     items: [
       {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
-      },
-      {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
-      },
-      {
-        name: "",
-        price: "",
-        description: "",
-        urls: "/",
+        name: "Emerald Elegance Gold Ring",
+        id: "emeraldRing",
+        price: "35",
+        description:
+          "Experience the allure of our Emerald Elegance Gold Ring. This exquisite piece features a series of rectangular green gemstones, possibly emeralds, set in a gold band. The gemstones are aligned side by side, creating a complete circle that exudes sophistication. Perfect for adding a touch of luxury to any outfit.",
+        urls: [
+          "/images/products/jewelry-accessories/emerald-ring-primary.jpg",
+          "/images/products/jewelry-accessories/emerald-ring-secondary.jpg",
+        ],
       },
     ],
   },
